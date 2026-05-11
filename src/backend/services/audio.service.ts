e3e23d7a7d236d86708adb02343aa32bc8e3838d
Orion CI <orion-ci@example.com>
2026-05-11 02:17:51 +0100

    ­ƒÜÇ Orion Auto-Update

--- src/backend/services/audio.service.ts
+++ src/backend/services/audio.service.ts
@@ -1,10 +1,6 @@
 import axios from 'axios';
 import fs from 'fs';
 import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export class AudioService {
     /**
